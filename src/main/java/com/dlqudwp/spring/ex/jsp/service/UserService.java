e77f6f8e34904ff784f74ca85bd8bf4266399371
dlqudwp <dlqudwp21@naver.com>
2023-08-28 18:52:50 +0900

    [이병제] 8.28 수업

---
+++ src/main/java/com/dlqudwp/spring/ex/jsp/service/UserService.java
@@ -0,0 +1,32 @@
+package com.dlqudwp.spring.ex.jsp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dlqudwp.spring.ex.jsp.domain.User;
+import com.dlqudwp.spring.ex.jsp.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public int addUser(String name, String birthday, String email) {
+		
+		
+		int count = userRepository.insertUser(name, birthday, email);
+		
+		return count;
+	}
+	
+	public User getLastUser() {
+		User user = userRepository.selectLastUser();
+		
+		return user;
+		
+	}
+	
+	
+}
